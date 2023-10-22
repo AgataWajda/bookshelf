fe000f13a1e719f65d19504fe6b2887728483f4c
AgataWajda <127791105+AgataWajda@users.noreply.github.com>
2023-10-22 09:04:58 +0000

    Deploying to gh-pages from @ AgataWajda/bookshelf@1e7f5c47c70a696d843288e17c2e710e5302402b 🚀

---
+++ index.e61eccdc.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".pop-up-modal"),o=document.querySelector(".backdrop-modal");document.querySelector("#allBooks");document.querySelector("button").addEventListener("click",(function(){e.classList.remove("is-hidden"),o.classList.remove("is-hidden")}));
+//# sourceMappingURL=index.e61eccdc.js.map

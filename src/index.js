c72dfd38dbb5717ff5656a9c2fac8b78e9679e8c
ArturBaur <artur.baur@gmail.com>
2023-10-22 19:27:09 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import './sass/main.scss';
+import './js/categories-menu';
---
+++ src/js/bookshelf-api.js
@@ -0,0 +1,14 @@
+const axios = require('axios').default;
+const API_URL = 'https://books-backend.p.goit.global/books/';
+
+export default class BookshelfApi {
+  async fetchCategoriesList() {
+    try {
+      const url = API_URL + 'category-list';
+      const response = await axios.get(url);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}

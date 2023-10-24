a17d3376f3d976b4684e3abdcde1c0399aa58df4
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-10-24 19:58:55 +0200

    add class Bookshelf

--- src/index.js
+++ src/index.js
@@ -1,27 +1,50 @@
 import './sass/main.scss';
 import './pop-up-modal';
 import './js/categories-menu';
-import { fetchBooksByCategory, fetchBooksCategoryList } from './js/bookshelf-api';
+import { fetchBooksByCategory, fetchBooksCategoryList, Bookshelf } from './js/bookshelf-api';
 
-async function booksByCategory(selectedCategory) {
-  const data = await fetchBooksByCategory(selectedCategory);
-  const arrBooks = data.map((item) => {
-    const book = {
-      author: item.author,
-      book_image: item.book_image,
-      description: item.description,
-      category: item.list_name,
-      publisher: item.publisher,
-      price: item.price,
-      marketAmazon: item.amazon_product_url,
-      rank: item.rank,
-    };
-    return book;
-  });
-  return arrBooks;
-}
+// async function booksByCategory(selectedCategory) {
+//   const data = await fetchBooksByCategory(selectedCategory);
+//   const arrBooks = data.map((item) => {
+//     const book = {
+//       title: item.title,
+//       author: item.author,
+//       book_image: item.book_image,
+//       description: item.description,
+//       category: item.list_name,
+//       publisher: item.publisher,
+//       price: item.price,
+//       marketAmazon: item.amazon_product_url,
+//       rank: item.rank,
+//     };
+//     return book;
+//   });
+//   console.log('data: ', data);
+//   console.log('arrBooks: ', arrBooks);
+//   return arrBooks;
+// }
 
-booksByCategory('Business Books');
+// booksByCategory('Business Books');
+
+
+const  my_Bookshelf = new Bookshelf()
+// my_Bookshelf.fetchByCategory('Business Books');
+console.log('Category - Business book: ', my_Bookshelf.fetchByCategory('Business Books').then(data => {
+  return data.map((item) => {
+  const book = {
+    title: item.title,
+    author: item.author,
+    book_image: item.book_image,
+    description: item.description,
+    category: item.list_name,
+    publisher: item.publisher,
+    price: item.price,
+    marketAmazon: item.amazon_product_url,
+    rank: item.rank,
+  };
+  return book;
+});
+}));
 
 async function booksCategoryList() {
   const data = await fetchBooksCategoryList();

d2a900705ca83c39acf37bfb8225976772ba724f
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-10-26 01:00:31 +0200

    fix: fetchById()

--- src/index.js
+++ src/index.js
@@ -1,37 +1,13 @@
 import './sass/main.scss';
 import './pop-up-modal';
 import './js/categories-menu';
-import { fetchBooksByCategory, fetchBooksCategoryList, Bookshelf } from './js/bookshelf-api';
-
-// async function booksByCategory(selectedCategory) {
-//   const data = await fetchBooksByCategory(selectedCategory);
-//   const arrBooks = data.map((item) => {
-//     const book = {
-//       title: item.title,
-//       author: item.author,
-//       book_image: item.book_image,
-//       description: item.description,
-//       category: item.list_name,
-//       publisher: item.publisher,
-//       price: item.price,
-//       marketAmazon: item.amazon_product_url,
-//       rank: item.rank,
-//     };
-//     return book;
-//   });
-//   console.log('data: ', data);
-//   console.log('arrBooks: ', arrBooks);
-//   return arrBooks;
-// }
-
-// booksByCategory('Business Books');
-
+import { Bookshelf } from './js/bookshelf-api';
 
 const  my_Bookshelf = new Bookshelf()
-// my_Bookshelf.fetchByCategory('Business Books');
-console.log('Category - Business book: ', my_Bookshelf.fetchByCategory('Business Books').then(data => {
-  return data.map((item) => {
+ my_Bookshelf.fetchByCategory('Business Books').then(data => {
+  const books = data.map((item) => {
   const book = {
+    _id: item._id,
     title: item.title,
     author: item.author,
     book_image: item.book_image,
@@ -44,12 +20,57 @@ console.log('Category - Business book: ', my_Bookshelf.fetchByCategory('Business
   };
   return book;
 });
-}));
+console.log('Business Books: ', books);
+return books;
+});
 
-async function booksCategoryList() {
-  const data = await fetchBooksCategoryList();
-  const arrListName = await data.map((element) => element.list_name);
-  console.log('Category List: ', arrListName);
-}
 
-booksCategoryList();
+const bookslist = my_Bookshelf.fetchByCategory('Audio Nonfiction').then(data => {
+  const books = data.map((item) => {
+  const book = {
+    _id: item._id,
+    title: item.title,
+    author: item.author,
+    book_image: item.book_image,
+    description: item.description,
+    category: item.list_name,
+    publisher: item.publisher,
+    price: item.price,
+    marketAmazon: item.amazon_product_url,
+    rank: item.rank,
+  };
+  return book;
+});
+console.log('Audio Nonfiction: ', books);
+return books;
+});
+
+
+my_Bookshelf.fetchBooksCategoryList().then((data) => {
+  const list = data.map((element) => element.list_name);
+  console.log('Category List: ', list);
+});
+
+my_Bookshelf.fetchById("643282b1e85766588626a0b6").then((data) => {
+  const book = {
+      _id: data._id,
+      title: data.title,
+      author: data.author,
+      book_image: data.book_image,
+      description: data.description,
+      category: data.list_name,
+      publisher: data.publisher,
+      price: data.price,
+      marketAmazon: data.amazon_product_url,
+      rank: data.rank,
+    };
+    console.log('book by id: ', book);
+    return book;
+});
+
+
+my_Bookshelf.fetchTopBooks().then((data) => {
+  // const list = data.map((element) => element.list_name);
+  console.log('Top List: ', data);
+});
+

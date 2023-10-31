b4de2d0a7d1e3090599b0175249f1d174f8bd78e
ArturBaur <artur.baur@gmail.com>
2023-10-31 02:44:37 +0100

    update

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,28 +1,25 @@
-document.querySelector('.switch').addEventListener('click', event => {
-  event.preventDefault();
-  if (localStorage.getItem('theme') === 'dark') {
+const switchElement = document.querySelector('.switch');
+const body = document.querySelector('body');
+
+function updateTheme() {
+  const isDarkMode = localStorage.getItem('theme') === 'dark';
+  body.classList.toggle('dark', isDarkMode);
+  switchElement.classList.toggle('checked', isDarkMode);
+}
+
+function toggleDarkMode() {
+  const isDarkMode = localStorage.getItem('theme') === 'dark';
+  if (isDarkMode) {
     localStorage.removeItem('theme');
   } else {
     localStorage.setItem('theme', 'dark');
   }
-  addDarkClassToHTML();
-});
-
-function addDarkClassToHTML() {
-  try {
-    const body = document.querySelector('body');
-    const switchElement = document.querySelector('.switch');
-
-    if (localStorage.getItem('theme') === 'dark') {
-      body.classList.add('dark');
-      switchElement.classList.add('dark');
-    } else {
-      body.classList.remove('dark');
-      switchElement.classList.remove('dark');
-    }
-  } catch (err) {
-    console.error(err);
-  }
+  updateTheme();
 }
 
-addDarkClassToHTML();
+switchElement.addEventListener('click', event => {
+  event.preventDefault();
+  toggleDarkMode();
+});
+
+updateTheme();
